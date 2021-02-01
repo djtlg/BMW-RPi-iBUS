ea07e6c58b1f8a920cd9f6eea7f0841237574fd6
tolgaildiz <ildiztolga@gmail.com>
2021-02-01 00:19:14 -0400

    Initial commit of installation script.

--- install.sh
+++ install.sh
@@ -9,17 +9,44 @@ echo
 echo ===  Installing Necessary Packages  ===
 echo
 apt-get update
-apt-get install -y python python-dbus python-gobject-2 python-serial python-paho-mqtt alsa-utils bluez bluez-tools pulseaudio pulseaudio-module-bluetooth
+apt-get install -y python python-dbus python-gobject-2 python-serial python-paho-mqtt alsa-utils bluez bluez-tools pulseaudio pulseaudio-module-bluetooth ofono
 apt autoremove
 
 echo
-echo ===  Creating the user  ===
+echo "===  Creating \"bmw\" user if doesnt exist  ==="
 echo
 id -u bmw &>/dev/null || useradd bmw --groups bluetooth --create-home
+
+echo
+echo "=== Adding \"pulse\" user to \"bluetooth\" group  ==="
+echo
 usermod -a -G bluetooth pulse
 
 echo
-echo ===  Copying script files  ===
+echo "=== Adding \"bmw\" user to \"pulse-access\" group  ==="
+echo
+usermod -a -G pulse-access bmw
+
+echo
+echo "=== Adding \"pi\" user to \"pulse-access\" group  ==="
+echo
+usermod -a -G pulse-access pi
+
+echo
+echo "=== Adding \"root\" user to \"pulse-access\" group  ==="
+echo
+usermod -a -G pulse-access root
+
+#TODO Create a user for ofono and make sure bmw is part of it
+#edit /etc/dbus/system.d/ofono.conf and add the following
+# <!-- allow users of bluetooth group to communicate -->
+#  <policy group="bluetooth">
+#    <allow send_destination="org.ofono"/>
+#  </policy>
+
+
+echo
+echo "===  Copying script files  to \"/opt/bmw-agent\"==="
 echo
 
 mkdir -p /opt/bmw-agent
@@ -28,14 +55,19 @@ cp *.py /opt/bmw-agent
 chown -R bmw:bmw /opt/bmw-agent
 
 echo
-echo ===  Copying systemd scripts  ===
+echo "===  Copying systemd scripts  ==="
 echo
 cp ./etc/systemd/system/bmw-agent.service /etc/systemd/system
 cp ./etc/systemd/system/pulseaudio.service /etc/systemd/system
+cp ./etc/systemd/system/bluetooth.service.d/bluetooth.conf /etc/systemd/system/bluetooth.service.d
 
-systemctl daemon-reload
 echo
-echo ===  Make sure services start on startup  ===
+echo "===  Reload systemd  ==="
+echo
+systemctl daemon-reload
+
+echo
+echo "===  Make sure services start on startup  ==="
 echo
 systemctl enable bmw-agent.service
 systemctl enable pulseaudio.service
@@ -44,3 +76,8 @@ echo
 echo "===  Review the output and if no errors restart the device  ==="
 echo
 
+systemctl restart dbus
+systemctl restart ofono
+systemctl restart bluetooth
+systemctl restart pulseaudio
+systemctl restart bmw-agent
